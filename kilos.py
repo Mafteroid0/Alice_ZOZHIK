33c8a6985320dbd220333aa381c2cf5c1d3dbbe4
TypeHintsFun <mikhailsmnv22@gmail.com>
2023-03-24 10:54:15 +0800

    indev time parsing update

--- kilos.py
+++ kilos.py
@@ -1,4 +1,5 @@
 import datetime
+import functools
 
 import pymorphy2
 from pandas import to_timedelta
@@ -10,7 +11,9 @@ TIME_UNITS = {
     'ночь': 'h',
     'день': 'h',
     'пол': 'h',
-    'четверть': 'h'
+    'час': 'h',
+    'четверть': 'h',
+    'минута': 'm',
 }
 
 
@@ -20,10 +23,12 @@ def today() -> datetime.datetime:
                                      microseconds=time.microsecond)
 
 
+@functools.cache
 def normalize(word: str) -> str:
     return morph.parse(word)[0].normal_form
 
 
+@functools.cache
 def is_real_time(text: str) -> bool:
     text = text.replace(',', '.')
     text = text.replace(';', ':')
@@ -39,6 +44,7 @@ def clean(text: str) -> str:
 def parse_time(text: str) -> datetime.datetime:
     time = today()
 
+    text = text.replace('-', ':')
     if text.count(':') in (1, 2):  # Оптимизировать вызовы count
         if text.count(':') == 1:
             text = f'{text}:00'
@@ -46,84 +52,100 @@ def parse_time(text: str) -> datetime.datetime:
     elif text.count(':') > 2:
         raise ValueError()
 
-    text_l = clean(text).split()
+    temp = [[]]
+    index = 0
+    for word in clean(text).split():
+        if word == 'и':
+            index += 1
+            try:
+                temp[index]
+            except IndexError:
+                temp.append([])
+            continue
+        temp[index].append(word)
 
-    parsed: list[str] = []
+    del index
 
-    num_buf: str | None = None
-    txt_buf: str | None = None
-    for word_is_time, word in map(lambda x: (is_real_time(x), x), text_l):
-        parsed.append(word)
-        if not word_is_time:
-            word = normalize(word)
-            match word:
-                case 'ночь' | 'утро':
-                    if 'пол' in parsed or 'четверть' in parsed:
-                        print('-', datetime.timedelta(hours=12))
-                        time -= datetime.timedelta(hours=12)
-                case 'вечер' | 'день':
-                    if not ('пол' in parsed or 'четверть' in parsed):
-                        print('+', datetime.timedelta(hours=12))
+    for text_l in temp:
+        parsed: list[str] = []
+
+        num_buf: str | None = None
+        txt_buf: str | None = None
+        for word_is_time, word in map(lambda x: (is_real_time(x), x), text_l):
+            parsed.append(normalize(word))
+            print(1)
+            if not word_is_time:
+                print(2)
+                word = normalize(word)
+                match word:
+                    case 'ночь' | 'утро':
+                        if 'пол' in parsed or 'четверть' in parsed:
+                            print('1-', datetime.timedelta(hours=12))
+                            time -= datetime.timedelta(hours=12)
+                    case 'вечер' | 'день':
+                        if not ('пол' in parsed or 'четверть' in parsed):
+                            print('2+', datetime.timedelta(hours=12))
+                            time += datetime.timedelta(hours=12)
+                    case 'пол':
+                        print('3-', datetime.timedelta(minutes=30))
+                        time -= datetime.timedelta(minutes=30)
+                        print('4+', datetime.timedelta(hours=12))
                         time += datetime.timedelta(hours=12)
-                case 'час':
+                    case 'четверть':
+                        print('5-', datetime.timedelta(minutes=45))
+                        time -= datetime.timedelta(minutes=45)
+                        print('6+', datetime.timedelta(hours=12))
+                        time += datetime.timedelta(hours=12)
+
+            print([*parsed, txt_buf])
+            if 'минута' in [*parsed, txt_buf] and int(num_buf) < 12:
+                print('6.1+', datetime.timedelta(hours=12))
+                time += datetime.timedelta(hours=12)
+
+            if word_is_time or num_buf is not None and txt_buf is not None:
+                if txt_buf is None:
+                    num_buf = word
                     continue
-                case 'пол':
-                    print('-', datetime.timedelta(minutes=30))
-                    time -= datetime.timedelta(minutes=30)
-                    print('+', datetime.timedelta(hours=12))
-                    time += datetime.timedelta(hours=12)
-                case 'четверть':
-                    print('-', datetime.timedelta(minutes=45))
-                    time -= datetime.timedelta(minutes=45)
-                    print('+', datetime.timedelta(hours=12))
-                    time += datetime.timedelta(hours=12)
 
-        if word_is_time or num_buf is not None and txt_buf is not None:
-            if num_buf is not None:
-                word = num_buf
-
-            if txt_buf is None:
-                num_buf = word
+                print('7+', to_timedelta(f'{word}{TIME_UNITS[txt_buf]}'))
+                time += to_timedelta(f'{word}{TIME_UNITS[txt_buf]}')
+                txt_buf = None
+                num_buf = None
                 continue
 
-            print('+', to_timedelta(f'{word}{TIME_UNITS[txt_buf]}'))
-            time += to_timedelta(f'{word}{TIME_UNITS[txt_buf]}')
-            txt_buf = None
-            num_buf = None
-            continue
+            if (not word_is_time) or txt_buf is not None:
+                if txt_buf is not None:
+                    word = txt_buf
 
-        if (not word_is_time) or txt_buf is not None:
-            if txt_buf is not None:
-                word = txt_buf
+                match word:
+                    case 'полдень':
+                        time = today() + datetime.timedelta(hours=12)
+                        txt_buf = None
+                        num_buf = None
+                        continue
+                    case 'полночь':
+                        time = today()
+                        txt_buf = None
+                        num_buf = None
+                        continue
 
-            match word:
-                case 'полдень':
-                    time = today() + datetime.timedelta(hours=12)
-                    txt_buf = None
-                    num_buf = None
-                    continue
-                case 'полночь':
-                    time = today()
-                    txt_buf = None
-                    num_buf = None
+                if num_buf is None:
+                    txt_buf = word
                     continue
 
-            if num_buf is None:
-                txt_buf = word
+                print('8+', to_timedelta(f'{num_buf}{TIME_UNITS[word]}'))
+                time += to_timedelta(f'{num_buf}{TIME_UNITS[word]}')
+                txt_buf = None
+                num_buf = None
                 continue
 
-            print('+', to_timedelta(f'{num_buf}{TIME_UNITS[word]}'))
-            time += to_timedelta(f'{num_buf}{TIME_UNITS[word]}')
-            txt_buf = None
-            num_buf = None
-            continue
-
-    # Тут хендлим всё, что не попадает под модель "число указатель" или "указатель число"
-    if time == today() and num_buf is not None:
-        time += datetime.timedelta(
-            hours=int(num_buf))  # TODO: Опасное место, может правоцировать много ошибок. Нужно проработать
+        # Тут хендлим всё, что не попадает под модель "число указатель" или "указатель число"
+        if time == today() and num_buf is not None:
+            num_buf: int = int(num_buf)  # TODO: Опасное место, может правоцировать много ошибок. Нужно проработать
+            time += datetime.timedelta(
+                hours=num_buf if num_buf >= 12 else num_buf + 12)
 
     return time
 
 
-print(parse_time('3:00:00'))
+print(parse_time('3 часа 10 минут'))
